2cc4b18037f0707a4c50d103fd1e342869b359dd
eddie.hale <eddie.hale@gmail.com>
2021-09-22 18:15:47 +0000

    reverting demo changes

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,9 @@
 import { helloWorld } from './index';
 import expressRequestMock from 'express-request-mock'; // https://github.com/i-like-robots/express-request-mock
 
-test('index returns 200 and Hello World!', async () => {
+test('index returns 200 and expected text', async () => {
     const { res } = await expressRequestMock(helloWorld);
     
     expect(res.statusCode).toEqual(200);
-    expect(res._getData()).toBe('Go Hogs, beat Aggies!!');
+    expect(res._getData()).toBe('Hello World!');
 });
