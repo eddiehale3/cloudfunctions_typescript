46494d0b8adfc90d9796a968fe98a70e3ffd5566
eddie.hale <eddie.hale@gmail.com>
2021-09-25 13:53:11 +0000

    updated test case for response

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,5 +5,5 @@ test('index returns 200 and expected text', async () => {
     const { res } = await expressRequestMock(helloWorld);
     
     expect(res.statusCode).toEqual(200);
-    expect(res._getData()).toBe('Hello World!');
+    expect(res._getData()).toBe('Go Hogs, Beat Aggies!!');
 });
